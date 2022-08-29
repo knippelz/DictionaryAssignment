811e1e820273e87d1704c6a46cd865ddd469101e
tinyeye-zach <zachary.knippel@tinyeye.com>
2022-08-28 21:52:02 -0600

    Refactored logic into DictionaryTracker class, (in CompoundWordLibrary)

--- CompoundWordLibrary/CompoundWordLibrary.cs
+++ CompoundWordLibrary/CompoundWordLibrary.cs
@@ -1,4 +1,6 @@
 ﻿namespace DictionaryAssignmentLibraries;
+using System;
+using System.Linq;
 public static class CompoundWordLibrary
 {
     public static bool IsValidCompoundWord(string firstWord, string secondWord, int targetLength) {
@@ -26,5 +28,80 @@ public class CompoundWord
     public override string ToString()
     {
         return $"{firstWord} + {secondWord} => {compound()}";
+    }
+}
+
+public class DictionaryTracker
+{
+    public List<CompoundWord> finalWords { get; set;}
+    public string[] rawWords { get; set;}
+    public int targetLength { get; set;}
+    private Dictionary<int, List<string>> wordsByLength;
+    
+    public DictionaryTracker(string[] rawWords, int targetLength = 6)
+    {
+        this.targetLength = targetLength;
+        this.rawWords = rawWords;
+        this.finalWords = new List<CompoundWord>();
+        this.wordsByLength = new Dictionary<int, List<string>>();
+        this.generateCompoundWords();
+    }
+
+    private void sortWordsByLength()
+    {
+        foreach (string word in rawWords)
+        {
+            if (!wordsByLength.ContainsKey(word.Length))
+            {
+                List<string> init = new List<string>();
+                init.Add(word);
+                wordsByLength.Add(word.Length, init);
+            } else {
+                wordsByLength[word.Length].Add(word);
+            }
+        }
+    }
+
+    private void calculateCompoundWords()
+    {
+        foreach (KeyValuePair<int, List<string>> wordsList in this.wordsByLength)
+        {
+            int currentLength = wordsList.Key;
+            int neededLength = this.targetLength - currentLength;
+            if (currentLength > 0 && neededLength >= 1 && this.wordsByLength.ContainsKey(neededLength))
+            {
+                List<string> currentWords = wordsList.Value;
+                currentWords.ForEach(firstWord => {
+                    this.wordsByLength[neededLength].ForEach(secondWord => {
+                        CompoundWord newWord = new CompoundWord(firstWord, secondWord);
+                        this.finalWords.Add(newWord);
+                    });
+                });
+            }
+        }
+    }
+
+    private void sortFinalWords()
+    {
+        IEnumerable<CompoundWord> sortedWords = this.finalWords.OrderBy(word => word.compound()).ThenBy(word => word.firstWord.Length);
+        this.finalWords = sortedWords.ToList();
+        // System.IO.File.WriteAllLines($"finalWords.txt", sortedWords.ToList().ConvertAll(word => word.ToString()));
+    }
+
+    private void generateCompoundWords()
+    {
+        this.sortWordsByLength();
+        this.calculateCompoundWords();
+        this.sortFinalWords();
+    }
+
+    public List<string> outputList()
+    {
+        return this.finalWords.ToList().ConvertAll(word => word.ToString());
+    }
+
+    public void outputToConsole()
+    {
+        this.outputList().ForEach(word => Console.WriteLine(word.ToString()));
     }
 }
--- DictionaryAssignment/Program.cs
+++ DictionaryAssignment/Program.cs
@@ -9,45 +9,11 @@ class Program
     {
         try
         {
-            List<CompoundWord> finalWords = new List<CompoundWord>();
-            int targetLength = 6;
             string[] rawWords = System.IO.File.ReadAllLines("dictionary.txt");
-            Dictionary<int, List<string>> wordsByLength = new Dictionary<int, List<string>>();
+            DictionaryTracker dT = new DictionaryTracker(rawWords);
 
-            foreach (string word in rawWords)
-            {
-                if (!wordsByLength.ContainsKey(word.Length))
-                {
-                    List<string> init = new List<string>();
-                    init.Add(word);
-                    wordsByLength.Add(word.Length, init);
-                } else {
-                    wordsByLength[word.Length].Add(word);
-                }
-            }
-            
-            foreach (KeyValuePair<int, List<string>> wordsList in wordsByLength)
-            {
-                int currentLength = wordsList.Key;
-                int neededLength = targetLength - currentLength;
-                if (currentLength > 0 && neededLength >= 1 && wordsByLength.ContainsKey(neededLength))
-                {
-                    List<string> currentWords = wordsList.Value;
-                    currentWords.ForEach(firstWord => {
-                        wordsByLength[neededLength].ForEach(secondWord => {
-                            CompoundWord newWord = new CompoundWord(firstWord, secondWord);
-                            finalWords.Add(newWord);
-                        });
-                    });
-                }
-                // Console.WriteLine("---------");
-                // Console.WriteLine($"Array of Words with Length = {wordsList.Key}:");
-                // wordsList.Value.ForEach(word => Console.Write("{0},", word));
-                // System.IO.File.WriteAllLines($"wordsList_Length-{wordsList.Key}.txt", wordsList.Value);
-            }
+            dT.outputToConsole();
 
-            IEnumerable<CompoundWord> query = finalWords.OrderBy(word => word.compound()).ThenBy(word => word.firstWord.Length);
-            System.IO.File.WriteAllLines($"finalWords.txt", query.ToList().ConvertAll(word => word.ToString()));
         }
         catch (IOException e)
         {
